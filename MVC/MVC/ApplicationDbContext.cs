0b28f0b6844a01e40a544a16713fc6798d88e745
ShevchenkoDmytriy <dimasevcenko506@gmail.com>
2023-09-08 18:15:18 +0300

    Edit

--- MVC/MVC/ApplicationDbContext.cs
+++ MVC/MVC/ApplicationDbContext.cs
@@ -8,16 +8,8 @@ namespace MVC
         public DbSet<Users> Users { get; set; } = null!;
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
         {
-            Database.EnsureCreated();
         }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Users>().HasData(
-                new Users { Id=1,Name="qwqg",Age=23},
-                                new Users { Id = 2, Name = "qwqg", Age = 23 },
-                                                new Users { Id = 3, Name = "qwqg", Age = 23 }
-                );
-        }
+
     }
 
 }
