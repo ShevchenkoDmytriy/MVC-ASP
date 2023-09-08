b888bf073b9048b1cc04f238dc4c1d98eb476465
ShevchenkoDmytriy <dimasevcenko506@gmail.com>
2023-09-08 17:44:29 +0300

    ASP

---
+++ MVC/MVC/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC.Model;
+
+namespace MVC
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Users> Users { get; set; } = null!;
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            Database.EnsureCreated();
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Users>().HasData(
+                new Users { Id=1,Name="qwqg",Age=23},
+                                new Users { Id = 2, Name = "qwqg", Age = 23 },
+                                                new Users { Id = 3, Name = "qwqg", Age = 23 }
+                );
+        }
+    }
+
+}
---
+++ MVC/MVC/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace MVC.Controllers
+{
+        public class HomeController : Controller
+        {
+            private readonly ApplicationDbContext _context;
+
+            public HomeController(ApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<IActionResult> Index()
+            {
+                var users = await _context.Users.ToListAsync();
+                return View(users);
+            }
+        }
+}
